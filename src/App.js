c576d06fed5e1d87c01820206d7b5c11f4753cfc
Eliel Lima <elieldalbo@gmail.com>
2020-10-20 15:42:03 -0300

    Formatação dos dados

--- src/App.js
+++ src/App.js
@@ -1,57 +1,84 @@
 // cSpell:Ignore obtem
 
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-function App() {
-  const [economia, setEconomia] = useState([])
+export default function App() {
 
   useEffect (() => {
-    document.title = 'Finance Quotation'
+    obtemDados()
   },[])
 
+  const [dolar, setDolar] = useState([])
+  const [euro, setEuro] = useState([])
+  const [bitcoin, setBitcoin] = useState([])
+
+
   async function obtemDados(){
-    setEconomia([])
-    //setCarregando(true)
-    //const filtraEtnia = etnia.length > 0 ? `&ethnicity=${etnia}` : ''
-    //const filtraIdade = idade.length > 0 ? `&age=${idade}` : ''
-    let url = `https://economia.awesomeapi.com.br/json/all`
+    let url = `http://economia.awesomeapi.com.br/json/all`
+
     await fetch(url)
     .then(response => response.json())
     .then(data => {
-      const uri = data
-      console.log(data.USD)
-      uri && setEconomia(data.USD)
+      console.log(data)
+      setDolar(data.USD)
+      setEuro(data.EUR)
+      setBitcoin(data.BTC)
     })
     .catch(function (error){
-      console.error('Houve um problema ao efetuar a requisição: ' + error.message)
+      console.error(`Houve um erro: ${error}`)
     })
-    //setCarregando(false)
   }
-
-  function ListaDados(props) {
-    const economia = props.data
-    const listagemDados = economia.map((moeda) => 
-      <p>{moeda.name}</p>
-    );
-    return (
-    <ul>{listagemDados}</ul>
-    );
-}
 
   return (
     <div className="App">
-        <p>Teste App</p>
-        {economia.length > 0
-        ? <ListaDados economia={economia.USD} />
-        :  <p>Erro ao carregar os dados</p>
-        }
-        <button type="button" onClick={obtemDados}>
-        Finance Quotation
-      </button>
-
+        <h1>Finance Quotation</h1>
+        <table border="1">
+        <thead>
+          <tr>
+            <th>Variação</th>
+            <th>Valor Compra</th>
+            <th>Moeda</th>
+            <th>Mínimo</th>
+            <th>Máximo</th>
+          </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>{`${dolar.pctChange} %`}</td>
+          <td>{`R$ ${dolar.bid}`}</td>
+          <td>{`${dolar.name}`}</td>
+          <td><p><small>{`R$ ${dolar.low}`}</small></p></td>
+          <td>{`R$ ${dolar.high}`}</td>
+        </tr>
+        <tr>
+          <td>{`${euro.pctChange} %`}</td>
+          <td>{`R$ ${euro.bid}`}</td>
+          <td>{euro.name}</td>
+          <td>{`R$ ${euro.low}`}</td>
+          <td>{`R$ ${euro.high}`}</td>
+        </tr>
+        <tr>
+          <td>{`${bitcoin.pctChange} %`}</td>
+          <td>{`R$ ${bitcoin.bid}`}</td>
+          <td>{bitcoin.name}</td>
+          <td>{`R$ ${bitcoin.low}`}</td>
+          <td>{`R$ ${bitcoin.high}`}</td>
+        </tr>
+        </tbody>
+        </table>
+        <span class="align-baseline">
+          <h4>{`${dolar.name}`}</h4>
+          <p>{`${dolar.pctChange} %`}</p>
+          <p>{`R$ ${dolar.bid}`}</p>        
+          <p><small>{`Min. R$ ${dolar.low} / Max. R$ ${dolar.high}`}</small></p>
+        </span>
+        <span class="align-middle">
+          <h4>{`${euro.name}`}</h4>
+          <p>{`${euro.pctChange} %`}</p>
+          <p>{`R$ ${euro.bid}`}</p>        
+          <p><small>{`Min. R$ ${euro.low} / Max. R$ ${euro.high}`}</small></p>
+        </span>
     </div>
-  )
+  );
 }
-
-export default App;
