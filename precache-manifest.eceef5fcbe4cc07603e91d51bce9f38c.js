255c217d831f14ffd3bf2374d2328e59ded9042f
Eliel Lima <elieldalbo@gmail.com>
2020-10-24 12:46:33 -0300

    Updates

---
+++ precache-manifest.eceef5fcbe4cc07603e91d51bce9f38c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82acedcd61bbe71508b7d98643e5b41b",
+    "url": "/finance/index.html"
+  },
+  {
+    "revision": "942ee85dc31fcec86905",
+    "url": "/finance/static/css/main.b876e1d4.chunk.css"
+  },
+  {
+    "revision": "8b5565ee36c315b34b01",
+    "url": "/finance/static/js/2.5554d1f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/finance/static/js/2.5554d1f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "942ee85dc31fcec86905",
+    "url": "/finance/static/js/main.c3d2b197.chunk.js"
+  },
+  {
+    "revision": "a058ced0c4077819c0b5",
+    "url": "/finance/static/js/runtime-main.1aeee481.js"
+  }
+]);
